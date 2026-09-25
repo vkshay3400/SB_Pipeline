9995cf2d389089f4e6df9b0d39bc6375311d3863
vkshay3400 <akshayp2208@gmail.com>
2026-09-25 17:21:06 +0530

    first commit

---
+++ src/main/java/com/test/sbpipeline/SbpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.test.sbpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbpipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/sbpipeline/SbpipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sbpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbpipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
